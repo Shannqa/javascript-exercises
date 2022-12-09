29555b1e259d807f173de23928031eeea938ca61
Shannqa <116514532+Shannqa@users.noreply.github.com>
2022-12-09 20:46:17 +0100

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function([...numbers]) {
+  let total = 0;
+  for (const num of numbers) {
+    total += parseInt(num);
+  }
+
+	return total;
 };
 
-const multiply = function() {
+const multiply = function([...numbers]) {
+  const array = [...numbers];
+  let total = 0;
+  total = array.reduce(function(a, b) {
+    return parseInt(a) * parseInt(b);
+  });
+  return total;
+  }
 
+
+const power = function(a, b) {
+	let total = 0;
+  total = Math.pow(a, b); 
+  return total;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+	let total = 1;
+  for (let i = 1; i <= a; i++) {
+    total *= i; 
+  }
+  return total;
+}
 
 // Do not edit below this line
 module.exports = {
