e0b82fee2ca599a4f360e8097b7b8163b4e6958f
Shannqa <116514532+Shannqa@users.noreply.github.com>
2022-12-09 22:05:09 +0100

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let newString = string.toLowerCase().replace(/\W/g, '');
+  const array = newString.split('');
+  const reverse = array.reverse();
+  let reverseString = reverse.join('');
+  if (newString === reverseString) {
+    return true;
+    
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
