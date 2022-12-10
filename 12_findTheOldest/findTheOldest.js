3ee6cd0971320d9cd030f1d24b124dce78a58f55
Shannqa <116514532+Shannqa@users.noreply.github.com>
2022-12-11 00:10:15 +0100

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const currentYear = (new Date()).getFullYear();
+  const sortedByAge = people.sort(function(a, b) {
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = currentYear;
+    };
+    if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = currentYear;
+    };
+    const lastPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - b.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  });
+  return sortedByAge[0];
 };
 
 // Do not edit below this line
