0d60d7516df47ab17c95731f9872f94bb2782d99
Shannqa <116514532+Shannqa@users.noreply.github.com>
2022-12-10 23:24:19 +0100

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let num1 = 0;
+  let num2 = 1;
+  let result;
+  number = parseInt(number);
 
+  if (number === 0) {
+    return num1;
+  } else if (number === 1) {
+    return num2;
+  } else if (number > 1) {
+    for (let i = 2; i <= number; i++) {
+      result = num1 + num2;
+      num1 = num2;
+      num2 = result;
+    }
+    return result;
+  } else if (number < 1) {
+    return 'OOPS';
+  }
 };
 
 // Do not edit below this line
